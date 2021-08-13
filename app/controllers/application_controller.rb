965c1fd303286e08c201a3d26d57d05a51581dfe
Newburu <newburu@gmail.com>
2021-08-13 15:20:15 +0900

    Login Parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
 
+  # パラメータキー（ユーザコード）
+  PARAMSKEY_USER_CODE = "user_code"
+
   private
 
   def require_login
     unless logged_in?
-      session[:current_user_code] = request.headers[:HTTP_USER_CODE]
+      session[:current_user_code] = params[PARAMSKEY_USER_CODE]
+      session[:current_user_code] ||= request.headers[:HTTP_USER_CODE]
       @current_user = User.find_by(user_code: session[:current_user_code])
       # ユーザが存在しない場合は消す
       if @current_user.nil?
@@ -15,7 +19,8 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-    session[:current_user_code] == request.headers[:HTTP_USER_CODE]
+    ret = (session[:current_user_code] == params[PARAMSKEY_USER_CODE])
+    ret &&= (session[:current_user_code] == request.headers[:HTTP_USER_CODE])
   end
 
 end
