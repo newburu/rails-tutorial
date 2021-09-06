9a3e2f5f4510d5f3566ecba588350d780711bb11
Newburu <newburu@gmail.com>
2021-09-06 22:45:36 +0900

    only development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   def require_login
     unless logged_in?
-      session[:current_user_code] = params[PARAMSKEY_USER_CODE]
+      session[:current_user_code] = params[PARAMSKEY_USER_CODE] if Rails.env.development?
       session[:current_user_code] ||= request.headers[:HTTP_USER_CODE]
       @current_user = User.find_by(user_code: session[:current_user_code])
       # ユーザが存在しない場合は消す
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-    ret = (session[:current_user_code] == params[PARAMSKEY_USER_CODE])
+    ret = (session[:current_user_code] == params[PARAMSKEY_USER_CODE]) if Rails.env.development?
     ret &&= (session[:current_user_code] == request.headers[:HTTP_USER_CODE])
   end
 
